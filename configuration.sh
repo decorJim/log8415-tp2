2fab13197d8ff9d3f0c97f13f48c1e699c25eba6
Mohammed Ridha Ghoul <104877596+ridaghoul@users.noreply.github.com>
2023-11-17 17:41:19 -0500

    Optimize the configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -9,14 +9,5 @@ pip install pyspark;
 pip install findspark;
 echo "pyspark and findspark installed"
 
-# SSH setup
-apt install -y ssh;
-
-service ssh restart;
-
-ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa;
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys;
-chmod 0600 ~/.ssh/authorized_keys;
-
 mkdir /var/lib/hadoop;
 chmod 777 /var/lib/hadoop;
